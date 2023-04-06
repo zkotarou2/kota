1e003c2991496165872cdeee3f6cf41435e44534
Jan <lamjanming@outlook.com>
2023-04-06 16:58:18 +0200

    Added all towa songs

--- src/js/data/2023-04-06.js
+++ src/js/data/2023-04-06.js
@@ -1,20 +1,20 @@
-import {towaAAA} from './vtubers/towa.js'
+import {towaAAA} from './vtubers/towaAAA.js'
 
 dataSetVersion = "2023-04-06"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
-    {
-        name: "Filter by vtubers",
-        key: "singer",
-        tooltip: "Check this to show songs containing these vtubers",
-        checked: false,
-        sub: [
-            {name: "Yatogami Towa", key: "towa"},
-            {name: "Hoshimachi Suisei", key: "suisei"},
-            {name: "Minato Aqua", key: "aqua"}
-        ]
-    }
+    /* {
+         name: "Filter by vtubers",
+         key: "singer",
+         tooltip: "Check this to show songs containing these vtubers",
+         checked: false,
+         sub: [
+             {name: "Yatogami Towa", key: "Towa"},
+             {name: "Hoshimachi Suisei", key: "Suisei"},
+             {name: "Minato Aqua", key: "Aqua"},
+         ]
+     }*/
 ];
 
 dataSet[dataSetVersion].characterData = [
---
+++ src/js/data/vtubers/towaAAA.js
@@ -0,0 +1,310 @@
+export const towaAAA = [
+    {
+        name: "Adabana Necromancy",
+        videoId: "mFLu8TKDsrk",
+        opts: {
+            singer: ["Polka", "Aqua", "Towa", "Flare", "Watame", "Miko"]
+        }
+    },
+    {
+        name: "Akuma",
+        videoId: "F1_OQXNF8b8",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Asu no Yozora Shoukaihan",
+        videoId: "XpgSrULHibg",
+        opts: {
+            singer: ["Towa", "Aqua", "Suisei"]
+        }
+    },
+    {
+        name: "AsuiroClearSky",
+        embedLink: "2D0B3wTjE20",
+        opts: {
+            singer: ["Towa", "Flare", "Aki", "Mio", "Miko", "Aqua", "Matsuri", "Mel", "Roboco"]
+        }
+    },
+    {
+        name: "Blooming in The Mud",
+        videoId: "08J6WAmX100",
+        opts: {
+            singer: ["Towa", "Baelz"]
+        }
+    },
+    {
+        name: "Bocca della Verita",
+        videoId: "64kI2BWRd2g",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "born to be real",
+        videoId: "D6AmCNdwT7E",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "brilliant",
+        videoId: "2D0B3wTjE20",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Can you do the hololive? Beyond the Stage ver",
+        videoId: "mZ0sJQC8qkE",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Cat Loving",
+        videoId: "ShXmOdYGuG8",
+        opts: {
+            singer: ["Towa", "Okayu"]
+        }
+    },
+    {
+        name: "Cynical Night Plan",
+        videoId: "nWP8Wmho188",
+        opts: {
+            singer: ["Towa", "Reine"]
+        }
+    },
+    {
+        name: "Diamond Crevasse",
+        videoId: "7AQi222UFrA",
+        opts: {
+            singer: ["Towa", "AZKi"]
+        }
+    },
+    {
+        name: "ERROR",
+        videoId: "3UV8OZj2olg",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "EYE",
+        videoId: "6uN99OAINAY",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "FACT",
+        videoId: "Zc05le75CfI",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Gira Gira",
+        videoId: "XXuMwJIAHsY",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Haiiro to ao",
+        videoId: "0firv69LkgI",
+        opts: {
+            singer: ["Towa", "Suisei"]
+        }
+    },
+    {
+        name: "Hearts",
+        videoId: "wVfp1NPf928",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Hikaru Nara",
+        videoId: "zu0DHyWBwpM",
+        opts: {
+            singer: ["Towa", "Robocco", "Mel", "Lui", "Chloe"]
+        }
+    },
+    {
+        name: "Hyakkisai",
+        videoId: "2o_H09MdT94",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "KING",
+        videoId: "9RBY7u5qas8",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Kiseki Knot",
+        videoId: "k7cpjmkc1Rw",
+        opts: {
+            singer: ["Towa", "Kanata", "Luna", "Coco", "Watame"]
+        }
+    },
+    {
+        name: "Kuroi Hitsuji",
+        videoId: "ZQ3gmQzpvBM",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Kuusou Ressha",
+        videoId: "p1lfwyWSGV4",
+        opts: {
+            singer: ["Towa", "Aqua", "Suisei"]
+        }
+    },
+    {
+        name: "Like a Phoenix",
+        videoId: "TcwMO-QU3ak",
+        opts: {
+            singer: ["Towa", "Kanata"]
+        }
+    },
+    {
+        name: "Madoromi",
+        videoId: "23nEnPOXLEk",
+        opts: {
+            singer: ["Towa", "Kanata"]
+        }
+    },
+    {
+        name: "Midnight Runaway",
+        videoId: "MT5F4GzNNQw",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "My roar",
+        videoId: "uNQIltgLlgo",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Ochame Kinou",
+        videoId: "9kQ2GtvDV3s",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Over Time",
+        videoId: "5ArDbvfds9k",
+        opts: {
+            singer: ["Towa", "Kanata"]
+        }
+    },
+    {
+        name: "Palette",
+        videoId: "Ud73fm4Uoq0",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Pray",
+        videoId: "9uyK-EMEDgA",
+        opts: {
+            singer: ["Towa", "Kanata"]
+        }
+    },
+    {
+        name: "Prism Melody",
+        videoId: "1fX6qYFXemY",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "QUEEN",
+        videoId: "_8L2KJeoHGo",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Raimei",
+        videoId: "TfOx00AOfKo",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Remote Control",
+        videoId: "SVVyG36QwqI",
+        opts: {
+            singer: ["Towa", "Nene"]
+        }
+    },
+    {
+        name: "Sakura no Ame",
+        videoId: "D8pdSRs_C4o",
+        opts: {
+            singer: ["Towa", "Miko", "Aki", "Omaru", "Iroha"]
+        }
+    },
+    {
+        name: "Sanvitalia",
+        videoId: "knb_mosTUvo?start=3276",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Shiny Smily Story 2022 ver",
+        videoId: "i_Cm8Jbvc-4",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Stardust Song by",
+        videoId: "LiJBEm9v5Nw",
+        opts: {
+            singer: ["Towa", "Kanata", "Flare"]
+        }
+    },
+    {
+        name: "Tonde K! hololive summer",
+        videoId: "zHD5MCUezVo",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "U.F.O. - U Feel Overjoyed!",
+        videoId: "HBmss54l5zc",
+        opts: {
+            singer: ["Towa"]
+        }
+    },
+    {
+        name: "Usseewa",
+        videoId: "S-VngJTUdmE",
+        opts: {
+            singer: ["Towa", "Kanata"]
+        }
+    },
+    {
+        name: "Yokusou to Neon Tetra",
+        videoId: "HM_El_RDfLY",
+        opts: {
+            singer: ["Towa", "Okayu"]
+        }
+    }
+];
