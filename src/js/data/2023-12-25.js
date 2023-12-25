d2c07b396e6f846c18b0331bbb8f2e3d483302d7
janmlam <54781898+janmlam@users.noreply.github.com>
2023-12-25 08:56:57 +0000

    Add favicon and anime ops for ranking

---
+++ src/js/data/2023-12-25.js
@@ -0,0 +1,25 @@
+import {towaAAA} from './vtubers/towaAAA.js'
+import {animeOpenings} from "./misc/animeOps.js";
+
+dataSetVersion = "2023-12-25"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+    {
+        name: "Filter by vtubers or miscellaneous",
+        key: "singer",
+        tooltip: "Check this to show songs containing these vtubers",
+        checked: true,
+        sub: [
+            {name: "Tokoyami Towa", key: "Towa", checked: false},
+            {name: "Hoshimachi Suisei", key: "Suisei", checked: false},
+            {name: "Minato Aqua", key: "Aqua", checked: false},
+            {name: "Anime Openings", key: "AnimeOps", checked: true},
+        ]
+    }
+];
+
+dataSet[dataSetVersion].characterData = [
+    ...towaAAA,
+    ...animeOpenings
+];
---
+++ src/js/data/misc/animeOps.js
@@ -0,0 +1,506 @@
+export const animeOpenings = [
+    {
+        name: "Air - Tori no Uta - Lia",
+        videoId: "VlkxpXYpAZs",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Arifureta Shokugyou de Sekai Saikyou - FLARE - Void_Chords feat.LIO",
+        videoId: "eZN3afhTyGY",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Asobi Asobase - Three Piece - Konomi Kohara, Hina Kino, Rika Nagae",
+        videoId: "xX6R_TTnPEA",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "BNA - Ready To - Morohoshi Sumire",
+        videoId: "3Okzra8Anr4",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Boku Dake ga Inai Machi - Re:Re:  - Asian Kung-Fu Generation",
+        videoId: "fodAJ-1dN3I",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Bokura no Ameiro Protocol - S9aiR - SennaRin",
+        videoId: "tsNhwrj8xQQ",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "C³ - CubexCursedxCurious - Endless Story - Tamura Yukari",
+        videoId: "zLi3XHUNeUQ",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Carnival Phantasm - Super☆Affection - CooRie, Faylan, Hashimoto Miyuki, Kuribayashi Minami, Misato Aki, yozuca*",
+        videoId: "62c7_zKoSV8",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "ChaoS;HEAd - FDD - Itou Kanako",
+        videoId: "h9y2bw8xGGY",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Code Geass  - Colors - Flow",
+        videoId: "G8CFuZ9MseQ",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Death Parade - Flyers - Bradio",
+        videoId: "UjjTMNDZi-A",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Fairy tail - Snowy Fairy - FUNKIST",
+        videoId: "9jvVBVcZ0-Y",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Fate/stay night: unlimited blade works - Brave Shine - Aimer",
+        videoId: "7vZp3yGxZXE",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Fate/zero - Oath sign - LiSA",
+        videoId: "aK2-jBqwEWU",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Fate/zero - To the Beginning - Kalafina",
+        videoId: "hkQ8Rs5KXRw",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Fire Force - Inferno - Mrs. GREEN APPLE",
+        videoId: "JBqxVX_LXvk",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Fullmetal Alchemist - Ready steady go - L'Arc~en~Ciel",
+        videoId: "nYDcadIKSUg",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Fullmetal Alchemist  - Rewrite - ASIAN KUNG-FU GENERATION",
+        videoId: "oPg1J79sgkQ",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Gintama - Sakuramitsutsuki - SPYAIR",
+        videoId: "iXpGVuaQwXo",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Gosick - Destin Histoire - yoshiki*lisa",
+        videoId: "iOrkj_MFXm8",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Grand Blue - Grand Blue - Shounanno Kaze",
+        videoId: "0PpTUtHtwt4",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Highschool of the Dead - HIGHSCHOOL OF THE DEAD - Kishida Kyoudan & The Akeboshi Rockets",
+        videoId: "h4vDYcZwfHI",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Hikari no Go - Fantasy - Nana katase",
+        videoId: "H_DatietDwo",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Himouto Umaru-chan - Kakushinteki Metamaruphose - Tanaka Aimi",
+        videoId: "b6-2P8RgT0A",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Inuyasha - Change the World - V6",
+        videoId: "KwCfHVGamn8",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "JoJo's Bizarre Adventure Part 5: Golden Wind - Traitor's Requiem (Final version) - Daisuke Hasegawa",
+        videoId: "6rX1EtxwLbk",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Kantai collection - Miiro - Akino",
+        videoId: "Z3Gmv4_HkN4",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Katekyou Hitman Reborn! - Last Cross - Masami Mitsuoka",
+        videoId: "XZLoSHeZYEg",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Katekyou Hitman Reborn!  - Boys & Girls - LM.C",
+        videoId: "w3nb7mWuSBM",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Kimi ni Todoke - Kimi ni Todoke - Tomofumi Tanizawa",
+        videoId: "yN8Rlp3Oi94",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Kono Subarashii Sekai ni Shukufuku o! - Fantastic dreamer - Machico",
+        videoId: "fpG3BPNQepY",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Kyou Kara Maoh! - Hateshinaku Tooi Sora ni - THE STAND UP",
+        videoId: "Dopu91V0mOw",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Kyoukai no Kanata - Kyoukai no Kanata - Minori Chihara",
+        videoId: "JCQ_Xjaz5ek",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Link Clink - Dive Back in Time - BaiSha Jaws",
+        videoId: "w_DUwS3H5FE",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Lycoris Recoil - ALIVE - ClariS",
+        videoId: "VxR_BYPG7v4",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Magical Girl Lyrical Nanoha StrikerS -  Secret Ambition - Nana Mizuki",
+        videoId: "QisptZEIPTA",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Mahou Shoujo Madoka Magica - Connect - ClariS",
+        videoId: "OrgpX-_bFqM",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Mahou Shoujo Madoka Magica Movie 3: Hangyaku no Monogatari - Colorful - ClariS",
+        videoId: "2tmNzRnpwAs",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Minami-ke - Keikenchi Joushouchuu - Rina Satou, Marina Inoue, Minori Chihara",
+        videoId: "0EuJxX3ii-Q",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Mushoku Tensei Season 2 - spiral - LONGMAN",
+        videoId: "hkcdLR_tdtA",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Naruto - Yura Yura - Hearts Grow",
+        videoId: "-m8kR0FQ7Wk",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Naruto - GO!!! - Flow",
+        videoId: "xjBTNbEXbAA",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Nekopara (TV) - Shiny Happy Days - Yaki Yuki, Saeki Iori, Izawa Shiori, Ito Miku, Inoguchi Yuri, Mizutani Marine",
+        videoId: "Law190jES5I",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "No game no life - This game - Konomi Suzuki",
+        videoId: "6CBp4qylX6I",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Noragami Aragoto - Kyouran Hey Kids!! - The Oral Cigarettes",
+        videoId: "aZenmeRytEM",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "One piece - Hikari E - BabyStars",
+        videoId: "BaZXGFnGei8",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "One Piece - Brand New World - D-51",
+        videoId: "Tyr7Ymbtl2Y",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "OnePiece/One Piece Film: Red - New Genesis - Ado",
+        videoId: "rB1c1pjFbWA",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Ore no Imouto ga Konna ni Kawaii Wake ga Nai - Irony - ClariS",
+        videoId: "UwlG5lCV_IE",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Parasyte - Let Me Hear - Fear, and Loathing in Las Vegas",
+        videoId: "Rm8UjBAS3cs",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Princess Connect! Re:Dive - Lost Princess - M・A・O, Miku Itō, Rika Tachibana",
+        videoId: "foImVnHpZDI",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Psycho pass - Abnormalize - Ling tosite sigure",
+        videoId: "5diaZzBr4_M",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Rainbow: Nisha Rokubou no Shichinin - We're Not Alone - Coldrain",
+        videoId: "ibkhGFKhyW8",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Re:ZERO -Starting Life in Another World- Season 2 - Realize - Konomi Suzuki",
+        videoId: "w-RUTZBbGwM",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Sekai Saikou no Ansatsusha, Isekai Kizoku ni Tensei Suru - Dark Seeks Light - Yui Ninomiya",
+        videoId: "u0rfZS13e_o",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Senki Zesshou Symphogear G - Vitalization - Nana Mizuki",
+        videoId: "806Wse-soqs",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Shakugan no Shana III - Serment - Mami Kawada",
+        videoId: "ILOfO22kysk",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "SSSS.Dynazenon - Imperfect - Masayoshi Oishi",
+        videoId: "j6N2DV3lclY",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Star Driver - Shining Star - 9nine",
+        videoId: "uSv4lUPShF4",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Strike the Blood : Valkyria no Ōkoku-hen - Little Charm - Yuka Iguchi",
+        videoId: "QShFxOyEgdY",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Tari Tari - Dreamer - Airi",
+        videoId: "JGouSdkRATI",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Tiger & Bunny -  Orion wo Nazoru - UNISON SQUARE GARDEN",
+        videoId: "sSfR3tCcXvg",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Toaru Majutsu no Index II - See visionS - Kawada Mami",
+        videoId: "HRnbHAUgoMc",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Tokyo Ghoul - Unravel - TK",
+        videoId: "7aMOurgDB-o",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Tokyo Ravens - X-encounter  - Maon kurosaki",
+        videoId: "g-HcGIino8g",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Tonikawa - Koi no Uta - Akari Kito",
+        videoId: "vQiodbKEW6s",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Utawarerumono: Itsuwari no kamen - Ame Kakeru Hoshi - Suara",
+        videoId: "GSwLYANb3nI",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Vinland Saga Season 2 - River - Anonymouz",
+        videoId: "honOJloqesA",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Ya Boy Kongming! - Chitty Chitty Bang Bang - QUEENDOM",
+        videoId: "gNn9NxZH2Vo",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Yahari Ore no Seishun Love Comedy wa Machigatteiru  - Yukitoki - Yanagi Nagi",
+        videoId: "0YIS96UqJRc",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Yahari Ore no Seishun Love Comedy wa Machigatteiru Zoku - Harumodoki - Yanagi Nagi",
+        videoId: "UxG3u4_523c",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    },
+    {
+        name: "Zero no Tsukaima: Futatsuki no Kishi - I SAY YES - Ichiko",
+        videoId: "8dx4ewFve_k",
+        opts: {
+            singer: ["AnimeOps"]
+        }
+    }
+];
