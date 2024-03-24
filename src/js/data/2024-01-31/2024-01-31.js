1d3ccc5b89e8c8523a32458e30ea5dd50d48b64e
ChuchuComete <160599605+ChuchuComete@users.noreply.github.com>
2024-03-24 15:39:17 +0100

    Update 2024-01-31.js

--- src/js/data/2024-01-31/2024-01-31.js
+++ src/js/data/2024-01-31/2024-01-31.js
@@ -3,6 +3,20 @@ import {animeEndings} from "./misc/animeEds.js";
 dataSetVersion = "2024-01-31"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
+dataSet[dataSetVersion].options = [
+    {
+        name: "Filter by type of songs",
+        key: "singer",
+        tooltip: "Check this to show songs",
+        checked: true,
+        sub: [
+            {name: "INS", key: "INS", checked: true},
+            {name: "OP", key: "OP", checked: true},
+            {name: "ED", key: "ED", checked: true}
+        ]
+    }
+];
+
 dataSet[dataSetVersion].characterData = [
     ...animeEndings
 ];
