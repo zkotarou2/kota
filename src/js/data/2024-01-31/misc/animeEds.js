4240bb1c6e76f4738ee2d7b427cf5de041e4bca9
ChuchuComete <160599605+ChuchuComete@users.noreply.github.com>
2024-03-24 15:10:53 +0100

    Update animeEds.js

--- src/js/data/2024-01-31/misc/animeEds.js
+++ src/js/data/2024-01-31/misc/animeEds.js
@@ -1,6 +1,6 @@
 export const animeEndings = [
 	{
-    name: "Bleach - Houkiboshi by Younha",'
+    name: "Bleach - Houkiboshi by Younha",
     videoId : "https://ladist1.catbox.video/gqlj8v.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -10,7 +10,7 @@ export const animeEndings = [
     }
 },
 	{
-    name: "Jyu-Oh-Sei - Te o Tsunaide by Younha",'
+    name: "Jyu-Oh-Sei - Te o Tsunaide by Younha",
     videoId : "https://ladist1.catbox.video/gb6rtf.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -20,7 +20,7 @@ export const animeEndings = [
     }
 },
 	{
-    name: "Kiba - Hakanaku Tsuyoku by Younha",'
+    name: "Kiba - Hakanaku Tsuyoku by Younha",
     videoId : "https://ladist1.catbox.video/4b5uh2.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -30,7 +30,7 @@ export const animeEndings = [
     }
 },
 	{
-    name: "Rideback - Kioku by Younha feat GOKU",'
+    name: "Rideback - Kioku by Younha feat GOKU",
     videoId : "https://ladist1.catbox.video/dx7nni.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -39,12 +39,12 @@ export const animeEndings = [
         ]
     }
 },{
-    name: "MapleStory - Scratch On The Heart by Younha",'
-    videoId : https://ladist1.catbox.video/vpulvc.webm,
+    name: "MapleStory - Scratch On The Heart by Younha",
+    videoId : "https://ladist1.catbox.video/vpulvc.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
-        OP 1
+        "OP 1"
         ]
     }
 }
