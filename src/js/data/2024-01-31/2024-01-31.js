89345aa1d2158108d23fe3922ba194efa51d5400
ChuchuComete <160599605+ChuchuComete@users.noreply.github.com>
2024-03-24 15:27:59 +0100

    Update 2024-01-31.js

--- src/js/data/2024-01-31/2024-01-31.js
+++ src/js/data/2024-01-31/2024-01-31.js
@@ -1,5 +1,3 @@
-import {towaAAA} from './vtubers/towaAAA.js'
-import {animeOpenings} from "./misc/animeOps.js";
 import {animeEndings} from "./misc/animeEds.js";
 
 dataSetVersion = "2024-01-31"; // Change this when creating a new data set version. YYYY-MM-DD format.
