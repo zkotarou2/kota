176578b4c608cf35fb8d2f07b46c62dc2840597f
zKotarou <Lars.Stoermer@hotmail.com>
2024-12-18 18:45:36 +0100

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -18,5 +18,165 @@ export const songs = [
         "OP"
       ]
     }
+  },
+  {
+    name: "7th Time Loop: The Villainess Enjoys a Carefree Life Married to Her Worst Enemy!",
+    videoId: "\"Another Birthday\" by Shunichi Toki",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "A Condition Called Love",
+    videoId: "\"Kimi no Sei\" by Sexy Zone",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "A Journey Through Another World: Raising Kids While Adventuring",
+    videoId: "\"Yururing Travel days\" by Tebasaki Sensation",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "A Nobody's Way Up to an Exploration Hero",
+    videoId: "\"Up Start\" by Amatsuki",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "A Salad Bowl of Eccentrics",
+    videoId: "\"Gif ni Ted\" by WANUKA",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "A Sign of Affection",
+    videoId: "\"Yuki no Ne\" by Novelbright",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Alya Sometimes Hides Her Feelings in Russian",
+    videoId: "\"1-ban Kagayaku Hoshi\" by Sumire Uesaka",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "An Archdemon's Dilemma: How to Love Your Elf Bride",
+    videoId: "\"Wakaranai Ai\" by The Brow Beat",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "As a Reincarnated Aristocrat, I'll Use My Appraisal Skill to Rise in the World",
+    videoId: "\"Blue Days\" by TRUE",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Astro Note",
+    videoId: "\"Hohoemi no Oto\" by Ai Furihata",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Atri: My Dear Moments",
+    videoId: "1\"Ano Hikari\" by Nogizaka46",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Banished from the Hero's Party, I Decided to Live a Quiet Life in the Countryside Season 2",
+    videoId: "\"routine life\" by Yui Nishio",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Bartender Glass of God",
+    videoId: "\"Stardust Memory\" by Takaya Kawasaki",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Black Butler: Public School Arc",
+    videoId: "\"Kyoushinsha no Parade - The Parade of Battlers\" by otoha",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Blue Archive The Animation",
+    videoId: "\"Seishun no Archive\" by Abydos Koutougakkou Taisaku Iinkai",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Blue Exorcist: Shimane Illuminati Saga",
+    videoId: "\"Eye's Sentry\" by UVERworld",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
   }
 ];
