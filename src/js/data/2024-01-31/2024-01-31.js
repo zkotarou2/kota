e3e8881876e74ef3e9d8db845f64ffc3a899f073
zkotarou2 <animeztakashi@gmail.com>
2025-12-23 17:31:24 +0100

    Update 2024-01-31.js

--- src/js/data/2024-01-31/2024-01-31.js
+++ src/js/data/2024-01-31/2024-01-31.js
@@ -1,22 +1,202 @@
-import {songs} from "./misc/songs.js";
-
-dataSetVersion = "2024-01-31"; // Change this when creating a new data set version. YYYY-MM-DD format.
-dataSet[dataSetVersion] = {};
-
-dataSet[dataSetVersion].options = [
-    {
-        name: "Filter by type of songs",
-        key: "singer",
-        tooltip: "Check this to show songs",
-        checked: true,
-        sub: [
-            {name: "INS", key: "INS", checked: true},
-            {name: "OP", key: "OP", checked: true},
-            {name: "ED", key: "ED", checked: true}
-        ]
+export const songs = [
+  {
+    name: "Vinland Saga - \"MUKANJYO\" by Survive Said The Prophet",
+    videoId: "Z4b70ecDelY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
     }
-];
-
-dataSet[dataSetVersion].characterData = [
-    ...songs
+  },
+  {
+    name: "Banana Fish - \"found & lost\"   by Survive Said The Prophet",
+    videoId: "VekCU4YJiR8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Vinland Saga Season 2 - \"Paradox\"   by Survive Said The Prophet",
+    videoId: "0YT3NTiZKDw",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Right and Left\"   by Survive Said The Prophet",
+    videoId: "_B6Pg5Ri7oQ",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Inside / Your Head\"   by Survive Said The Prophet",
+    videoId: "CtsK3wrqm2A",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Spectrum\"   by Survive Said The Prophet",
+    videoId: "3WJ8CJVVgl8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"S P I N E\"   by Survive Said The Prophet",
+    videoId: "PtiBBkEiHsY",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Conscious\"   by Survive Said The Prophet",
+    videoId: "B1PpXw1VN-k",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Tokyo 24th Ward - \"Papersky\"   by Survive Said The Prophet",
+    videoId: "RolH6t8NOro",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Bridges\"   by Survive Said The Prophet",
+    videoId: "mA3C0VcMdgA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Mary\"   by Survive Said The Prophet",
+    videoId: "Bt49cLLHVWE",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Useless feat. Ryo Kinoshita\"   by Survive Said The Prophet",
+    videoId: "XVrWwYQNaVA",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Beauty Queen\"   by Survive Said The Prophet",
+    videoId: "pQoJgfPKCis",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Code Geass Remake Movie - \"NE:ONE\"   by Survive Said The Prophet",
+    videoId: "https://naedist.animemusicquiz.com/lkjzs8.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Banana Fish - \"RED\"   by Survive Said The Prophet",
+    videoId: "_rU86Gl7bP8",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "Tokyo 24th Ward - \"Find You\"   by Survive Said The Prophet",
+    videoId: "https://naedist.animemusicquiz.com/bnpod7.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"3 A.M.\"   by Survive Said The Prophet",
+    videoId: "jPMOGEe7SQ0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"If You Really Want To\"   by Survive Said The Prophet",
+    videoId: "JC7K19rWL-k",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Mirror\"   by Survive Said The Prophet",
+    videoId: "vAuzMAcuQu0",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  },
+  {
+    name: "nan - \"Win / Lose\"   by Survive Said The Prophet",
+    videoId: "BUFiuDW0XDI",
+    videoSource: VIDEOSOURCE.YOUTUBE,
+    opts: {
+      singer: [
+        "OP"
+      ]
+    }
+  }
 ];
